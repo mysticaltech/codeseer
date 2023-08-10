90591169bf14857b5bf10e9a21bbb1027ebb3030
Karim Naufal <rimkashox@gmail.com>
2023-08-10 16:44:34 +0200

    updated chroma

--- create_embeddings.py
+++ create_embeddings.py
@@ -1,6 +1,5 @@
 import json
-from chromadb import Client
-from chromadb.config import Settings
+from chromadb import PersistentClient
 from chromadb.utils import embedding_functions
 import os
 from dotenv import load_dotenv
@@ -18,9 +17,7 @@ ef = embedding_functions.OpenAIEmbeddingFunction(
 )
 
 # Create a ChromaDB client and pass the ef
-client = Client(
-    Settings(chroma_db_impl="duckdb+parquet", persist_directory="db")
-)
+client = PersistentClient(path="db")
 
 # Create a collection to store the data
 collection = client.create_collection("code_snippets", embedding_function=ef)
@@ -28,9 +25,7 @@ collection = client.create_collection("code_snippets", embedding_function=ef)
 # Loop through each data point
 for idx, d in enumerate(data):
     # Save the metadata
-    metadata = {
-        k: ",".join(v) if isinstance(v, list) else v for k, v in d.items()
-    }
+    metadata = {k: ",".join(v) if isinstance(v, list) else v for k, v in d.items()}
 
     print(f"Adding data point {idx}: {metadata}")
 
@@ -41,13 +36,9 @@ for idx, d in enumerate(data):
             embeddings=None,
             metadatas=[metadata],
             documents=[d["code"]],
-            increment_index=True,
         )
         print(f"Data point {idx} added successfully.")
     except ValueError as e:
         print(f"Error adding data point {idx}: {e}")
     except Exception as e:
         print(f"Unexpected error adding data point {idx}: {e}")
-
-# Persist the data on disk
-client.persist()
